f15d81384dbdd8d2996163d49b9c1ab9d7800f44
FilinSky <daniel.filinski@gmail.com>
2025-07-08 11:37:28 +0300

    Исправлен отступ для More в таблице посредством изменения ширины столбца

--- src/pages/documents/components/DocumentsTable.tsx
+++ src/pages/documents/components/DocumentsTable.tsx
@@ -225,7 +225,7 @@ export const DocumentsTable: React.FC<{
                 </TableHeaderCell>
               </>
             )}
-            <TableHeaderCell style={{ width: '50px' }}>
+            <TableHeaderCell style={{ width: '80px' }}>
               <Text weight="semibold">More</Text>
             </TableHeaderCell>
           </TableRow>
